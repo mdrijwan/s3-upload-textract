35fa8736ebd724bd1516b7ef24f7b9fbb0c4a65d
mdrijwan <mrrmmonty@gmail.com>
2022-02-01 20:38:54 +0800

    Moving common functionalities to helpers

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -1,5 +1,12 @@
+import {
+  s3Upload,
+  analyzeProcess,
+  formatResponse,
+  detectProcess,
+  extractProcess,
+} from '../helpers/common'
+import { documentDataModel, extractedDataModel, s3ParamsModel } from '../helpers/model'
 import * as textractHelper from 'aws-textract-helper'
-import { s3Upload, documentExtract, formatResponse } from '../helpers/common'
 
 let response
 
@@ -8,7 +15,7 @@ export const handler = async (event) => {
     const parsedBody = JSON.parse(event.body)
     const base64File = parsedBody.file
     const decodedFile = Buffer.from(base64File.replace(/^data:image\/\w+;base64,/, ''), 'base64')
-    const params = {
+    const params: s3ParamsModel = {
       Bucket: process.env.UPLOAD_BUCKET,
       Key: `images/${new Date().toISOString()}.jpeg`,
       Body: decodedFile,
@@ -17,33 +24,23 @@ export const handler = async (event) => {
 
     const uploadResult = await s3Upload(params)
     console.log('UPLOAD RESULT', uploadResult)
-    const textractData = await documentExtract(uploadResult.Key)
+    const textractData = await analyzeProcess(uploadResult.Key)
     // console.log('TEXT DATA', textractData)
 
     const formData = textractHelper.createForm(textractData, { trimChars: [':', ' '] })
-    console.log('FORM DATA', JSON.stringify(formData))
-    // function trim (data) {
-    //     return data.replace(/(^\s+|\s+$)/g, '')
-    // }
+    // console.log('FORM DATA', JSON.stringify(formData))
 
-    const extractedData = {
-      transacation: formData.Transaction.trimEnd(),
-      model: formData.Model.trimEnd(),
-      issueDate: formData['Date of Issue'].trimEnd(),
-      registrationDate: formData['Date registered as Owner'].trimEnd(),
-      taxExemptWarranty: formData['Value of Unexpired Portion of Tax-exempt Warranty'].trimEnd(),
-      manufactureYear: formData['Year of Manufacture'].trimEnd(),
-      registrationMark: formData['Registration Mark'].trimEnd(),
-      class: formData.Class.trimEnd(),
-      taxValue: formData['First Registration Taxable Value'].trimEnd(),
-      chassis: formData[''].trimEnd(),
-    }
-    console.log('EXTRACT DATA', JSON.stringify(extractedData))
+    const extractedData: extractedDataModel = await extractProcess(formData)
+    // console.log('EXTRACT DATA', extractedData)
+
+    const testData: documentDataModel = await detectProcess(uploadResult.Key)
+    // console.log('TEST DATA', testData)
 
     response = {
       message: 'Successfully uploaded file to S3',
       uploadResult,
       extractedData,
+      testData,
     }
 
     return formatResponse(200, response)
